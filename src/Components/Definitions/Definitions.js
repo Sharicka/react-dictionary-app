8cd27b35bbe3fe4c951c3b8d55134a0b40be026a
Sharicka <sharickassc@gmail.com>
2023-09-18 11:59:50 -0500

    adding and displaying meaning, examples, synonyms

--- src/Components/Definitions/Definitions.js
+++ src/Components/Definitions/Definitions.js
@@ -7,7 +7,31 @@ const Definitions = ({ word, meanings, category }) => {
       {word === "" ? (
         <span className="Subtitle">Start by typing a word in Search</span>
       ) : (
-        meanings.map((mean) => mean.meanings.map((item) => console.log(item)))
+        meanings.map((mean) =>
+          mean.meanings.map((item) =>
+            item.definitions.map((def) => (
+              <div
+                className="singleMeaning"
+                style={{ backgroundColor: "white", color: "black" }}
+              >
+                <b>{def.definition}</b>
+                <hr style={{ backgroundColor: "black", width: "100%" }} />
+                {def.example && (
+                  <span>
+                    <b>Example : </b>
+                    {def.example}
+                  </span>
+                )}
+                {def.synonyms && (
+                  <span>
+                    <b>Synonyms : </b>
+                    {def.synonyms.map((s) => `${s},`)}
+                  </span>
+                )}
+              </div>
+            ))
+          )
+        )
       )}
     </div>
   );
